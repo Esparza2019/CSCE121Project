7a8d2a146b3faf8438beb5d57174d959a66004f3
Esparza2019 <dlef53@tamu.edu>
2016-04-05 12:57:06 -0500

    Added try and catch
    created the skeleton for our main

---
+++ project_main.cpp
@@ -0,0 +1,18 @@
+#include "std_lib_facilities_4.h"
+#include "Simple_window.h"
+#include "Graph.h"
+
+int main()
+try {
+    
+    
+return 0;
+}
+catch(exception& e) {
+	cerr << "exception: " << e.what() << '\n';
+	return 1;
+}
+catch (...) {
+	cerr << "Some exception\n";
+	return 2;
+}
